d48b002baf49ba1892ba50f57bc6c5a283a79378
thinkspill <github@thinkspill.org>
2015-02-10 11:30:45 -0800

    Remove hacky preg_match methods
    The tokenize method seems much more reliable.

--- src/FindMethodExceptions.php
+++ src/FindMethodExceptions.php
@@ -7,15 +7,20 @@ class FindMethodExceptions
 {
     protected $matches = [];
 
+    /**
+     * Find exceptions
+     * @param string $class
+     * @param string|bool $method_name
+     **/
     public function find($class, $method_name = false)
     {
         if (!class_exists($class)) {
             return "Class not found";
         }
-        $reflection = new ReflectionClass($class);
         if ($method_name) {
             $this->analyze_method($class, $method_name);
         } else {
+            $reflection = new ReflectionClass($class);
             $methods = $reflection->getMethods();
             foreach ($methods as $method) {
                 $this->analyze_method($class, $method->name);
@@ -25,8 +30,8 @@ class FindMethodExceptions
     }
 
     /**
-     * @param $class
-     * @param $method
+     * @param string $class
+     * @param string $method
      */
     private function analyze_method($class, $method)
     {
@@ -40,17 +45,16 @@ class FindMethodExceptions
         $endline = $reflected_method->getEndLine();
         $source = '';
         for ($i = $startline; $i <= $endline; $i++) {
-            //$this->try_preg_match_all($method, $file, $i);
             $source .= $file[$i];
         }
-        $this->try_tokenizing($method, $source);
+        $this->tokenize($method, $source);
     }
 
     /**
-     * @param $method
-     * @param $source
+     * @param string $method
+     * @param string $source
      */
-    private function try_tokenizing($method, $source)
+    private function tokenize($method, $source)
     {
         $source = "<?php " . trim($source, " {}\r\n") . " ?>";
 
@@ -80,40 +84,5 @@ class FindMethodExceptions
         }
 
         $this->matches[$method] = $res;
-    }
-
-    /**
-     * @param $method
-     * @param $file
-     * @param $line_number
-     */
-    private function try_preg_match_all($method, $file, $line_number)
-    {
-        $line = trim($file[$line_number]);
-        if ($matches = $this->find_matches($line)) {
-            if (empty($this->matches[$method])) {
-                $this->matches[$method] = [];
-            }
-            $this->matches[$method] = array_merge($this->matches[$method], $matches);
-        }
-    }
-
-    /**
-     * @param $line
-     * @return mixed
-     */
-    private function find_matches($line)
-    {
-        if (strpos($line, 'throw') === false) return false;
-        $matches = [];
-        $res = [];
-        preg_match_all('/(.*throw[\n\r ]new[\n\r ]([_a-z0-9]*)[($a-z)]*;)+/iU', $line, $matches, PREG_SET_ORDER);
-        foreach ($matches as $m) {
-            $res[] = $m[2];
-        }
-
-        if (count($res)) {
-            return $res;
-        } else return false;
     }
 }
